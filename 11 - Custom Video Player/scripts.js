3c0ab110938011dc263155a9d58c7895e1e1a0b0
sebasb718 <sebasb718@gmail.com>
2019-07-18 11:29:19 -0500

    clase 11, terminado con función de fullscreen

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,6 +4,7 @@ const video =  player.querySelector('.viewer');
 const progress =  player.querySelector('.progress');
 const progressBar =  player.querySelector('.progress__filled');
 const toggle =  player.querySelector('.toggle');
+const fullScreenButton =  player.querySelector('.fullScreen');
 const skipButtons =  player.querySelectorAll('[data-skip]');
 const ranges =  player.querySelectorAll('.player__slider');
 let mouseDown = false;
@@ -44,6 +45,15 @@ function scrub(e) {
   video.currentTime = scrub;
 }
 
+function fullScreen() {
+  if (document.fullscreen) {
+    document.exitFullscreen();
+  }
+  else{
+    player.requestFullscreen();
+  }
+}
+
 //Asociar los listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', toggleButton);
@@ -57,3 +67,4 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
 progress.addEventListener('mousedown', () => mouseDown = true);
 progress.addEventListener('mouseup', () => mouseDown = false);
+fullScreenButton.addEventListener('click', fullScreen);
